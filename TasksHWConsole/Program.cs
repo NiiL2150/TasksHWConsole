d427cfa167c4ea149402132cb6c1e5d7d5cd5099
Nail Shamsudinov <nail.shamsudinov@gmail.com>
2021-12-12 21:58:49 +0400

    Add project files.

---
+++ TasksHWConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TasksHWConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("REMINDER: Copy from:");
+            Console.WriteLine("MultiThreading");
+            Console.WriteLine("ThreadsHW");
+            Console.WriteLine("repos/proj or do yourself if can't find");
+        }
+    }
+}
